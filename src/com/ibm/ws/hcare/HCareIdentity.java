f4f1eeba63009b1e815ec74c46626f309840cd70
Scott W Graham <swgraham@us.ibm.com>
2015-09-08 06:52:55 -0600

    Add logging of LTPA Token

--- src/com/ibm/ws/hcare/HCareIdentity.java
+++ src/com/ibm/ws/hcare/HCareIdentity.java
@@ -6,11 +6,13 @@ import java.security.Principal;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.ibm.json.java.JSONObject;
+import com.ibm.websphere.security.web.WebSecurityHelper;
 
 /**
  * Servlet implementation class HCareIdentity
@@ -28,10 +30,10 @@ public class HCareIdentity extends HttpServlet {
 		
 		if (principal != null){
 			System.out.println ("HCareIdentity: doGet: user name = " + principal.getName());
-			
+			System.out.println ("HCareIdentity: doGet: token = " + getSecurityTokenLiberty());
 			JSONObject messageObject = new JSONObject();
 			messageObject.put("userid", principal.getName());
-	
+			messageObject.put("ltpaToken", getSecurityTokenLiberty());
 			response.setContentType("application/javascript");
 			response.setCharacterEncoding( "UTF-8" );
 			PrintWriter out = response.getWriter();
@@ -42,4 +44,25 @@ public class HCareIdentity extends HttpServlet {
 			System.out.println ("HCareIdentity: doGet: ERROR: user name = null");
 		}
 	}
+	
+	/*
+	*  Sample code to retrieve LTPA token on the IBM WebSphere Liberty Profile */
+	/* @return an LTPA token */
+	public  String getSecurityTokenLiberty() {
+		Cookie cookie = null;
+		String token = null;
+		try {
+			cookie = WebSecurityHelper.getSSOCookieFromSSOToken();
+			if (cookie != null) {
+				token = cookie.getValue();
+			}
+		} catch (Exception e) {
+			token = "no token found";
+			e.printStackTrace();
+		}
+		return token;
+	}
+	
+	
+	
 }
